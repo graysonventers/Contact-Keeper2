56c3ef12a6e7a1894910a8fbc6acb1b2669253fc
Grayson Venters <graysonventers@gmail.com>
2020-09-03 12:52:18 -0700

    express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,18 @@
+// create our express server
+const express = require('express');
+// initialize express server into app variable
+const app = express();
+
+// add endpoint/route
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+
+// create PORT variable for the express server (in app variable) to listen
+const PORT = process.env.PORT || 5000;
+// Add listen method onto express server to listen for the PORT variable. Call back function tells it what to do when it finds PORT.
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
